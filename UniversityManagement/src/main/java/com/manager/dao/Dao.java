dad6ac99d7bf121859026ced4a1f0341857cc220
Bohdan Kudrenko <bohdan.k20.04@gmail.com>
2025-06-15 23:20:56 +0200

    Commit

--- UniversityManagement/src/main/java/com/manager/dao/Dao.java
+++ UniversityManagement/src/main/java/com/manager/dao/Dao.java
@@ -3,10 +3,7 @@ package com.manager.dao;
 import java.sql.SQLException;
 import java.util.List;
 
-/**
- * Generic interface for Data Access Objects (DAO).
- * @param <T> The type of object the DAO works with (e.g., Student).
- */
+
 public interface Dao<T> {
     void add(T entity) throws SQLException;
     T getById(int id) throws SQLException;
